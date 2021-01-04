419863fcc3221d67142e6b39ddac44c08adfad60
陳亮瑜 <david80531.cs08g@nctu.edu.tw>
2021-01-04 08:16:42 -0800

    add IP test

--- context/upf_test.go
+++ context/upf_test.go
@@ -1,71 +1,80 @@
 package context_test
 
-// import (
-// 	"fmt"
-// 	"net"
-// 	"testing"
+import (
+	"fmt"
+	"net"
+	"testing"
 
-// 	"bitbucket.org/free5gc-team/nas/nasMessage"
-// 	"bitbucket.org/free5gc-team/smf/context"
-// 	. "github.com/smartystreets/goconvey/convey"
-// )
+	"bitbucket.org/free5gc-team/nas/nasMessage"
+	"bitbucket.org/free5gc-team/smf/context"
+	. "github.com/smartystreets/goconvey/convey"
+)
 
-// func TestIP(t *testing.T) {
+func convertPDUSessTypeToString(PDUtype uint8) string {
+	switch PDUtype {
+	case nasMessage.PDUSessionTypeIPv4:
+		return "PDU Session Type IPv4"
+	case nasMessage.PDUSessionTypeIPv6:
+		return "PDU Session Type IPv6"
+	case nasMessage.PDUSessionTypeIPv4IPv6:
+		return "PDU Session Type IPv4 IPv6"
+	case nasMessage.PDUSessionTypeUnstructured:
+		return "PDU Session Type Unstructured"
+	case nasMessage.PDUSessionTypeEthernet:
+		return "PDU Session Type Ethernet"
+	}
 
-// 	var testCases = []struct {
-// 		input               *context.UPFInterfaceInfo
-// 		inputPDUSessionType uint8
-// 		expectedIP          string
-// 		expectedError       error
-// 	}{
-// 		{
-// 			&context.UPFInterfaceInfo{
-// 				NetworkInstance:       "",
-// 				IPv4EndPointAddresses: []net.IP{net.ParseIP("8.8.8.8")},
-// 				IPv6EndPointAddresses: []net.IP{net.ParseIP("2001:4860:4860::8888")},
-// 				EndpointFQDN:          "www.google.com",
-// 			},
-// 			nasMessage.PDUSessionTypeIPv4,
-// 			"8.8.8.8",
-// 			nil,
-// 		},
-// 		{
-// 			&context.UPFInterfaceInfo{
-// 				NetworkInstance:       "",
-// 				IPv4EndPointAddresses: []net.IP{},
-// 				IPv6EndPointAddresses: []net.IP{},
-// 				EndpointFQDN:          "www.google.com",
-// 			},
-// 			nasMessage.PDUSessionTypeIPv4,
-// 			"8.8.8.8",
-// 			nil,
-// 		},
-// 	}
+	return "Unkwown PDU Session Type"
+}
 
-// 	Convey("Given different upf interface info structrues as follows", t, func() {
+func TestIP(t *testing.T) {
 
-// 		for i, testcase := range testCases {
+	var testCases = []struct {
+		input               *context.UPFInterfaceInfo
+		inputPDUSessionType uint8
+		expectedIP          string
+		expectedError       error
+	}{
+		{
+			&context.UPFInterfaceInfo{
+				NetworkInstance:       "",
+				IPv4EndPointAddresses: []net.IP{net.ParseIP("8.8.8.8")},
+				IPv6EndPointAddresses: []net.IP{net.ParseIP("2001:4860:4860::8888")},
+				EndpointFQDN:          "www.google.com",
+			},
+			nasMessage.PDUSessionTypeIPv4,
+			"8.8.8.8",
+			nil,
+		},
+		{
+			&context.UPFInterfaceInfo{
+				NetworkInstance:       "",
+				IPv4EndPointAddresses: []net.IP{net.ParseIP("8.8.8.8")},
+				IPv6EndPointAddresses: []net.IP{net.ParseIP("2001:4860:4860::8888")},
+				EndpointFQDN:          "www.google.com",
+			},
+			nasMessage.PDUSessionTypeIPv6,
+			"2001:4860:4860::8888",
+			nil,
+		},
+	}
 
-// 			upfInterfaceInfo := testcase.input
-// 			infoStr := fmt.Sprintf("tc[%d] UPF Interface Info: %+v", i, upfInterfaceInfo)
+	Convey("", t, func() {
+		for i, testcase := range testCases {
 
-// 			Convey(infoStr, func() {
-// 				Convey("select IPv4 pdusession type", func() {
-// 					ip, err := upfInterfaceInfo.input.IP(testcase.inputPDUSessionType)
-// 					Convey("IP addr should be "+testcase.expectedIP, func() {
-// 						So(testcase.expectedIP, ShouldEqual, ip.String())
-// 					})
+			upfInterfaceInfo := testcase.input
+			infoStr := fmt.Sprintf("testcase[%d] UPF Interface Info: %+v", i, upfInterfaceInfo)
 
-// 				})
+			Convey(infoStr, func() {
+				Convey("select "+convertPDUSessTypeToString(testcase.inputPDUSessionType), func() {
+					ip, err := upfInterfaceInfo.IP(testcase.inputPDUSessionType)
+					Convey("IP addr should be "+testcase.expectedIP, func() {
 
-// 				Convey("select IPv6 pdusession type", func() {
-
-// 					Convey("IP addr should be", nil)
-
-// 				})
-// 			})
-
-// 		}
-// 	})
-
-// }
+						So(ip.String(), ShouldEqual, testcase.expectedIP)
+						So(err, ShouldEqual, testcase.expectedError)
+					})
+				})
+			})
+		}
+	})
+}
