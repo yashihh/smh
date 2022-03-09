eddb4b6108de3c40ab39ac13a03b14ef5f90bd46
ycchen <chen042531.cs03@nctu.edu.tw>
2022-03-09 09:53:47 +0000

    [patch from free5GC]add test case to test IP pool allocate

--- internal/context/user_plane_information_test.go
+++ internal/context/user_plane_information_test.go
@@ -280,3 +280,232 @@ func TestSelectUPFAndAllocUEIP(t *testing.T) {
 		userplaneInformation.ReleaseUEIP(upf, allocatedIP, false)
 	}
 }
+
+var configForIPPoolAllocate = &factory.UserPlaneInformation{
+	UPNodes: map[string]*factory.UPNode{
+		"GNodeB": {
+			Type:   "AN",
+			NodeID: "192.168.179.100",
+		},
+		"UPF1": {
+			Type:   "UPF",
+			NodeID: "192.168.179.1",
+			SNssaiInfos: []*factory.SnssaiUpfInfoItem{
+				{
+					SNssai: &models.Snssai{
+						Sst: 1,
+						Sd:  "111111",
+					},
+					DnnUpfInfoList: []*factory.DnnUpfInfoItem{
+						{
+							Dnn: "internet",
+							Pools: []*factory.UEIPPool{
+								{
+									Cidr: "10.71.0.0/16",
+								},
+							},
+							StaticPools: []*factory.UEIPPool{
+								{
+									Cidr: "10.61.100.0/24",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"UPF2": {
+			Type:   "UPF",
+			NodeID: "192.168.179.2",
+			SNssaiInfos: []*factory.SnssaiUpfInfoItem{
+				{
+					SNssai: &models.Snssai{
+						Sst: 2,
+						Sd:  "222222",
+					},
+					DnnUpfInfoList: []*factory.DnnUpfInfoItem{
+						{
+							Dnn: "internet",
+							Pools: []*factory.UEIPPool{
+								{
+									Cidr: "10.62.0.0/16",
+								},
+							},
+							StaticPools: []*factory.UEIPPool{
+								{
+									Cidr: "10.62.100.0/24",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"UPF3": {
+			Type:   "UPF",
+			NodeID: "192.168.179.3",
+			SNssaiInfos: []*factory.SnssaiUpfInfoItem{
+				{
+					SNssai: &models.Snssai{
+						Sst: 3,
+						Sd:  "333333",
+					},
+					DnnUpfInfoList: []*factory.DnnUpfInfoItem{
+						{
+							Dnn: "internet",
+							Pools: []*factory.UEIPPool{
+								{
+									Cidr: "10.63.0.0/16",
+								},
+							},
+							StaticPools: []*factory.UEIPPool{
+								{
+									Cidr: "10.63.0.0/24",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	Links: []*factory.UPLink{
+		{
+			A: "GNodeB",
+			B: "UPF1",
+		},
+		{
+			A: "GNodeB",
+			B: "UPF2",
+		},
+		{
+			A: "GNodeB",
+			B: "UPF3",
+		},
+	},
+}
+
+var testCasesOfGetUEIPPool = []struct {
+	name          string
+	allocateTimes int
+	param         *UPFSelectionParams
+	subnet        uint8
+	useStaticIP   bool
+}{
+	{
+		name:          "static IP not in dynamic pool or static pool",
+		allocateTimes: 1,
+		param: &UPFSelectionParams{
+			Dnn: "internet",
+			SNssai: &SNssai{
+				Sst: 1,
+				Sd:  "111111",
+			},
+			PDUAddress: net.ParseIP("10.61.0.10"),
+		},
+		subnet:      61,
+		useStaticIP: false,
+	},
+	{
+		name:          "static IP not in static pool but in dynamic pool",
+		allocateTimes: 1,
+		param: &UPFSelectionParams{
+			Dnn: "internet",
+			SNssai: &SNssai{
+				Sst: 2,
+				Sd:  "222222",
+			},
+			PDUAddress: net.ParseIP("10.62.0.10").To4(),
+		},
+		subnet:      62,
+		useStaticIP: false,
+	},
+	{
+		name:          "dynamic pool is exhausted",
+		allocateTimes: 2,
+		param: &UPFSelectionParams{
+			Dnn: "internet",
+			SNssai: &SNssai{
+				Sst: 2,
+				Sd:  "222222",
+			},
+			PDUAddress: net.ParseIP("10.62.0.10").To4(),
+		},
+		subnet:      62,
+		useStaticIP: false,
+	},
+	{
+		name:          "static IP is in static pool",
+		allocateTimes: 1,
+		param: &UPFSelectionParams{
+			Dnn: "internet",
+			SNssai: &SNssai{
+				Sst: 3,
+				Sd:  "333333",
+			},
+			PDUAddress: net.ParseIP("10.63.0.10").To4(),
+		},
+		subnet:      63,
+		useStaticIP: true,
+	},
+	{
+		name:          "static pool is exhausted",
+		allocateTimes: 2,
+		param: &UPFSelectionParams{
+			Dnn: "internet",
+			SNssai: &SNssai{
+				Sst: 3,
+				Sd:  "333333",
+			},
+			PDUAddress: net.ParseIP("10.63.0.10").To4(),
+		},
+		subnet:      63,
+		useStaticIP: false,
+	},
+	{
+		name:          "static IP is in static pool, and dynamic pool is exhaust(allocate twice and not release)",
+		allocateTimes: 2,
+		param: &UPFSelectionParams{
+			Dnn: "internet",
+			SNssai: &SNssai{
+				Sst: 3,
+				Sd:  "333333",
+			},
+			PDUAddress: net.ParseIP("10.63.0.10").To4(),
+		},
+		subnet:      63,
+		useStaticIP: false,
+	},
+}
+
+func TestGetUEIPPool(t *testing.T) {
+	userplaneInformation := NewUserPlaneInformation(configForIPPoolAllocate)
+
+	for ci, tc := range testCasesOfGetUEIPPool {
+		t.Run(tc.name, func(t *testing.T) {
+			var expectedIPPool []net.IP
+			for i := 0; i <= 255; i++ {
+				for j := 1; j <= 255; j++ {
+					expectedIPPool = append(expectedIPPool, net.ParseIP(fmt.Sprintf("10.%d.%d.%d", tc.subnet, i, j)).To4())
+				}
+			}
+
+			var upf *UPNode
+			var allocatedIP net.IP
+			var useStatic bool
+			for times := 1; times <= tc.allocateTimes; times++ {
+				upf, allocatedIP, useStatic = userplaneInformation.SelectUPFAndAllocUEIP(tc.param)
+			}
+
+			require.Equal(t, tc.useStaticIP, useStatic)
+			// case 0 will not allocate IP
+			// case 2 and 4 which allocateTimes is 2 are used to test scenario which pool IP is exhausted
+			if ci == 0 || tc.allocateTimes > 1 {
+				require.Nil(t, allocatedIP)
+			} else {
+				require.Contains(t, expectedIPPool, allocatedIP)
+				userplaneInformation.ReleaseUEIP(upf, allocatedIP, tc.useStaticIP)
+			}
+		})
+	}
+}
